488f1accce81d46b133ed0aa997019ef5eec87cf
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2024-07-10 14:35:00 +0300

    fix edge

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge'
+
+export default async function NotFound() {
+
+    return (
+        <div className={"w-full h-screen"}>
+            <h1>Not found</h1>
+        </div>
+    )
+}
